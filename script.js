728f61e7a5ba764a31f9c736ab419200e452ab8a
Gosal Ram <gosalram.sg@techversantinfotech.com>
2024-10-10 18:33:56 +0530

    10/10/24

---
+++ script.js
@@ -0,0 +1,19 @@
+function qn1 (){
+
+document.getElementById('name').innerHTML="Hello World"
+
+}
+function qn11 (){
+
+document.getElementById('name').innerHTML="";
+
+}
+function sum(num1,num2){
+
+const a=document.getElementById('num1').value
+const b=document.getElementById('num2').value
+const c=Number(a)+Number(b)
+
+document.getElementById('sum').innerHTML="the sum is "+c
+
+}
